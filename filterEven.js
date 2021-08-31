876c4265104b73fd455af0c176aba0d0144845e8
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-31 10:53:09 -0400

    update console.log string

--- filterEven.js
+++ filterEven.js
@@ -5,11 +5,11 @@ const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const evenFilter = array => array.filter(e => e % 2 === 0);
 
-console.log("even numbers", evenFilter(numArray));
+console.log("even numbers are", evenFilter(numArray));
 
 // filtering an array and return numbers that are odd
 //
 
 const oddFilter = array => array.filter(e => e % 2 !== 0);
 
-console.log("odd numbers", oddFilter(numArray));
+console.log("odd numbers are ", oddFilter(numArray));
