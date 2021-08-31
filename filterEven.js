8b324310fcf88f2791bc29a758651a86b7713235
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-30 20:56:56 -0400

    returning true and false for each element

--- filterEven.js
+++ filterEven.js
@@ -1,5 +1,7 @@
 // filtering an array and return numbers that are even
-// 
+//
 
-const evenFilter = array => {}
+const evenFilter = (array) => array.map((e) => console.log(e % 2 === 0));
 
+
+evenFilter([1,2,3,4,5,6,7,8,9,10])
