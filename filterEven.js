b641a281a559ee6742790d31743b195ffa870ac1
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-31 09:34:26 -0400

    add odd filter

--- filterEven.js
+++ filterEven.js
@@ -1,8 +1,15 @@
 // filtering an array and return numbers that are even
-// 
+//
 
-const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenFilter = array => array.filter(e => e % 2 === 0)
+const evenFilter = (array) => array.filter((e) => e % 2 === 0);
 
-console.log(evenFilter(numArray));
+console.log("even numbers", evenFilter(numArray));
+
+// filtering an array and return numbers that are odd
+//
+
+const oddFilter = (array) => array.filter((e) => e % 2 !== 0);
+
+console.log("odd numbers", oddFilter(numArray));
