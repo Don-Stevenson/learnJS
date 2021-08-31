e2708ce4d3e78885cf15ec60787f8e1de8cecda7
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-31 09:35:35 -0400

    remove brackets on parameters

--- filterEven.js
+++ filterEven.js
@@ -3,13 +3,13 @@
 
 const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const evenFilter = (array) => array.filter((e) => e % 2 === 0);
+const evenFilter = array => array.filter(e => e % 2 === 0);
 
 console.log("even numbers", evenFilter(numArray));
 
 // filtering an array and return numbers that are odd
 //
 
-const oddFilter = (array) => array.filter((e) => e % 2 !== 0);
+const oddFilter = array => array.filter(e => e % 2 !== 0);
 
 console.log("odd numbers", oddFilter(numArray));
