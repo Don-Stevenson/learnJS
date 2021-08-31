039b0b1683baabb00b5c21fc3be112709eb29530
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-31 09:31:54 -0400

    create number array

--- filterEven.js
+++ filterEven.js
@@ -1,6 +1,8 @@
 // filtering an array and return numbers that are even
-//
+// 
+
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const evenFilter = array => array.filter(e => e % 2 === 0)
 
-console.log(evenFilter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(evenFilter(numArray));
