0e69c3fe21867904a3d5aa7c50c1586a10452d2e
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-30 21:01:00 -0400

    returns an array of even numbers

--- filterEven.js
+++ filterEven.js
@@ -1,7 +1,6 @@
 // filtering an array and return numbers that are even
 //
 
-const evenFilter = (array) => array.map((e) => console.log(e % 2 === 0));
+const evenFilter = array => array.filter(e => e % 2 === 0)
 
-
-evenFilter([1,2,3,4,5,6,7,8,9,10])
+console.log(evenFilter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
