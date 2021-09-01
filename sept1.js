8423593636f5b41c2e423e884d1efa6880f34f20
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-01 12:34:02 -0400

    remove parameter brackets

--- sept1.js
+++ sept1.js
@@ -2,7 +2,7 @@
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const twoX = (array) => {
+const twoX = array => {
   return array.map((e) => {
     if (e % 2 === 0) return 2 * e;
     else return e;
