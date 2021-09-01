8f6e23a6c796ff740665eb696381b444247b6a14
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-01 12:29:43 -0400

    add multiply by 2 if even

--- sept1.js
+++ sept1.js
@@ -1,7 +1,12 @@
-// use map to multiply the elements in an array by 2
+// use map to multiply the elements in an array by 2 if even only
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const twoX = array => array.map(e => 2 * e);
-
+const twoX = (array) => {
+  return array.map((e) => {
+    if (e % 2 === 0) return 2 * e;
+    if (e % 2 !== 0) return e;
+  });
+  
+};
 console.log(twoX(arr));
