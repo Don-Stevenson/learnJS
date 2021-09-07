47c7136450298d9320891e6da33a7a53fb13daa5
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-09-07 11:06:29 -0400

    remove parameter brackets and semi colons

--- sept1.js
+++ sept1.js
@@ -1,11 +1,11 @@
 // use map to multiply the elements in an array by 2 if even only
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const twoX = array => {
-  return array.map((e) => {
-    if (e % 2 === 0) return 2 * e;
-    else return e;
-  });
-};
-console.log(twoX(arr));
+  return array.map(e => {
+    if (e % 2 === 0) return 2 * e
+    else return e
+  })
+}
+console.log(twoX(arr))
