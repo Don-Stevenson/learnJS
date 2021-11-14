f13ed45b49c124ce01be7f819352743c30294eab
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-11-14 15:45:13 -0500

    Remove unnecessary brackets and semicolons

--- filterEven.js
+++ filterEven.js
@@ -1,15 +1,15 @@
 // filtering an array and return numbers that are even
 //
 
-const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const evenFilter = array => array.filter(e => e % 2 === 0);
+const evenFilter = array => array.filter(e => e % 2 === 0)
 
-console.log("even numbers are", evenFilter(numArray));
+console.log("even numbers are", evenFilter(numArray))
 
 // filtering an array and return numbers that are odd
 //
 
-const oddFilter = array => array.filter(e => e % 2 !== 0);
+const oddFilter = array => array.filter(e => e % 2 !== 0)
 
-console.log("odd numbers are ", oddFilter(numArray));
+console.log("odd numbers are ", oddFilter(numArray))
